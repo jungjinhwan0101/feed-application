593758570a2835c045518feec22991289fddc9d8
JungJinHwan <jinhjung@cuneo.co.kr>
2023-11-23 22:50:42 +0900

    feat: confirm code 모델 추가

--- apps/account/models.py
+++ apps/account/models.py
@@ -8,3 +8,9 @@ class User(AbstractUser):
         constraints = [
             models.UniqueConstraint(fields=["email"], name="email_unique"),
         ]
+
+
+class UserConfirmCode(models.Model):
+    user = models.OneToOneField('account.User', on_delete=models.CASCADE)
+    confirm_code = models.CharField(max_length=6, help_text='가입 승인 코드')
+    expired_at = models.DateTimeField()
